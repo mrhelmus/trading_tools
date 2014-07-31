7ba4109aa1ed50711830655398d3d4d102267465
mrhelmus <mrhelmus@gmail.com>
2014-07-31 14:20:33 -0400

    Get high frequency data
    code to get high frequency data

--- getHF.r
+++ getHF.r
@@ -1,11 +1,19 @@
 require(IBrokers)
-#require(quantmod)
-getHF<-function(contract,barSize,duration){
+getHF<-function(contract,barSize,duration,useRTH=0,endDateTime=NULL){
   twsConnect()->tws
   contract. <- twsEquity(contract)
-  dat<-reqHistoricalData(tws,contract.,barSize=barSize,duration=duration)
+  if(is.null(endDateTime)){
+    dat<-reqHistoricalData(tws,contract.,barSize=barSize,duration=duration,useRTH=useRTH,endDateTime=endDateTime)
+  } else {
+    dat<-reqHistoricalData(tws,contract.,barSize=barSize,duration=duration,useRTH=useRTH)
+  }
   twsDisconnect(tws)
   return(dat)
 }
+ll<-getHF('XIV',"1 hour","1 M",endDateTime="20140327 19:00:00")
+ll<-rbind(ll,getHF('XIV',"1 hour","1 M",endDateTime="20140427 19:00:00"))
+ll<-rbind(ll,getHF('XIV',"1 hour","1 M",endDateTime="20140527 19:00:00"))
+ll<-rbind(ll,getHF('XIV',"1 hour","1 M",endDateTime="20140627 19:00:00"))
+ll<-rbind(ll,getHF('XIV',"1 hour","1 M"))
 
-getHF('XIV',"1 week","1 Y")
+write.table(ll,"XIV.hr.txt")
